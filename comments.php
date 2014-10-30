cf4c622c77dc13b197966f784c9abc91e96fb762
Juan Camilo González <info@juancgonzalez.com>
2014-10-30 20:35:36 +0800

    change avatar size

--- comments.php
+++ comments.php
@@ -1,7 +1,4 @@
 <?php
-/*
-The comments page for jcg
-*/
 
 // don't load it if you can't comment
 if ( post_password_required() ) {
@@ -18,8 +15,8 @@ if ( post_password_required() ) {
         wp_list_comments( array(
           'style'             => 'div',
           'short_ping'        => true,
-          'avatar_size'       => 40,
-          'callback'          => jcg_comments,
+          'avatar_size'       => 80,
+          'callback'          => 'jcg_comments',
           'type'              => 'all',
           'reply_text'        => 'Reply',
           'page'              => '',
@@ -32,8 +29,8 @@ if ( post_password_required() ) {
 
     <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
     	<nav class="navigation comment-navigation" role="navigation">
-      	<div class="comment-nav-prev"><?php previous_comments_link( __( '&larr; Previous Comments', 'jcgtheme' ) ); ?></div>
-      	<div class="comment-nav-next"><?php next_comments_link( __( 'More Comments &rarr;', 'jcgtheme' ) ); ?></div>
+      	<div class="comment-nav-prev"><?php previous_comments_link( '&larr; Previous Comments' ); ?></div>
+      	<div class="comment-nav-next"><?php next_comments_link( 'More Comments &rarr;' ); ?></div>
     	</nav>
     <?php endif; ?>
 
