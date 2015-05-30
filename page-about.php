9435d27d534146e43fcc4947747beb81b582ad66
Juan Camilo Gonzalez <info@juancgonzalez.com>
2015-05-30 23:43:46 +0800

    create sections from array to avoid repeating tasks

--- page-about.php
+++ page-about.php
@@ -4,47 +4,47 @@
 */
 get_header();
 
-    $aboutData = (array)get_option('jcg_about_options');
-    $contentSeparator = ', ';
-    $dateformatstring = 'Y';
+  $aboutData = (array)get_option('jcg_about_options');
 
-    $profilePic = get_the_post_thumbnail($post->ID, 'large');
-    $authorName = get_bloginfo('name');
-    $bio         = !empty($aboutData['bio'])  ? apply_filters( 'the_content', $aboutData['bio'] ) : '';
-    $education   = new JCGCV('education', $contentSeparator, $dateformatstring);
-    $awards      = new JCGCV('awards', $contentSeparator, $dateformatstring);
-    $lectures    = new JCGCV('public-lectures-conferences', $contentSeparator, $dateformatstring);
-    $cultural    = new JCGCV('experience-in-cultural-initiatives', $contentSeparator, $dateformatstring);
-    $exEducation = new JCGCV('experience-in-education', $contentSeparator, $dateformatstring);
-    $exhibition  = new JCGCV('exhibitions', $contentSeparator, $dateformatstring);
+  $profilePic = get_the_post_thumbnail($post->ID, 'large');
+  $authorName = get_bloginfo('name');
+  $bio         = !empty($aboutData['bio'])  ? apply_filters( 'the_content', $aboutData['bio'] ) : '';
 
-    /*========================================
-    =            HEADER - PROFILE            =
-    ========================================*/
-    $profile = '<div id=profile class="cf">';
-      $profile .= '<div id="portrait">' . $profilePic . '</div>';
+  $cvSections = array(
+    'education',
+    'awards',
+    'experience-in-education',
+    'experience-in-cultural-initiatives',
+    'public-lectures-conferences',
+    'exhibitions',
+    'experience-in-animation-industry'
+  );
 
-      $profile .= '<div id="about-info">';
-        $profile .= '<h1>' . $authorName . '</h1>';
-        $profile .= jcg_contact_info();
+  /*========================================
+  =            HEADER - PROFILE            =
+  ========================================*/
+  $profile = '<div id=profile class="cf">';
+    $profile .= '<div id="portrait">' . $profilePic . '</div>';
 
-      $profile .= '</div>'; //end #about-info
-    $profile .= '</div>'; // end #profile
+    $profile .= '<div id="about-info">';
+      $profile .= '<h1>' . $authorName . '</h1>';
+      $profile .= jcg_contact_info();
 
-    echo $profile;
+    $profile .= '</div>'; //end #about-info
+  $profile .= '</div>'; // end #profile
 
-    /*-----  End of HEADER - PROFILE  ------*/
+  echo $profile;
 
-    $about = '<div id="about-content">';
-      $about .= '<div id="bio">' . $bio . '</div>';
-      $about .= $education->content;
-      $about .= $awards->content;
-      $about .= $exEducation->content;
-      $about .= $lectures->content;
-      $about .= $cultural->content;
-      $about .= $exhibition->content;
-    $about .= '</div>'; // end #about-content
+  /*-----  End of HEADER - PROFILE  ------*/
 
-    echo $about;
+  $about = '<div id="about-content">';
+    $about .= '<div id="bio">' . $bio . '</div>';
+    foreach ($cvSections as $section) {
+      $cvSection = new JCGCV($section);
+      $about .= $cvSection->content;
+    }
+  $about .= '</div>'; // end #about-content
+
+  echo $about;
 
 get_footer(); ?>
