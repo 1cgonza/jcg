696a7961ce3a7ce832b5c61366c12576063f9ae0
Juan Camilo González <info@juancgonzalez.com>
2018-10-13 20:48:10 -0500

    delete unused sidebar file

--- sidebar.php
+++
@@ -1,9 +0,0 @@
-  <aside id="sidebar1" class="sidebar m-all t-1of4 d-1of7 cf" role="complementary">
-
-    <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
-
-      <?php dynamic_sidebar( 'sidebar1' ); ?>
-
-    <?php endif; ?>
-
-  </aside>
