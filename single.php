cc3c0a22c15658335e9d8ce5eea8db654c8602f2
Juan Camilo González <info@juancgonzalez.com>
2018-10-14 14:10:08 -0500

    change ACF to CMB2

--- single.php
+++ single.php
@@ -2,20 +2,16 @@
   get_header();
   $postJS = '';
 
-    if ( have_posts() ) :
-      while ( have_posts() ) : the_post();
-        get_template_part( 'parts/content', get_post_type() );
+  if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+      get_template_part( 'parts/content', get_post_type() );
+      $postJS = get_post_meta($post->ID, '_jcg_js', true);
+    endwhile;
+  else :
+    get_template_part('parts/content', 'none');
+  endif;
 
-        $jsField = get_post_field('js', $post->ID, 'raw');
-        if ( !empty($jsField) ) {
-          $postJS = $jsField;
-        }
-      endwhile;
-    else :
-      get_template_part('parts/content', 'none');
-    endif;
-
-    if ( !empty($postJS) ) {
-      echo '<script type="text/javascript">' . $postJS . '</script>';
-    }
+  if ( !empty($postJS) ) {
+    echo '<script type="text/javascript">' . $postJS . '</script>';
+  }
   get_footer();
