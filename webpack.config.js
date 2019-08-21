97e8230761c02938b97a3805e5ef3f651585ef07
Juan Camilo González <info@juancgonzalez.com>
2019-08-21 00:31:24 -0500

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,69 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const WriteFilePlugin = require('write-file-webpack-plugin');
+const path = require('path');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              minimize: {
-                safe: true
-              }
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              autoprefixer: {
-                browsers: ['last 2 versions']
-              },
-              plugins: () => [
-                autoprefixer
-              ]
+module.exports = (env, options) => {
+  console.log(options.mode);
+  return {
+    devtool: options.mode === 'development' ? 'eval-source-map' : false,
+    devServer: {
+      contentBase: path.join(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader'
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {}
-          }
-        ]
-      },
-      {
-        test: /\.(ico|gif|png|jpe?g|svg)$/,
-        loaders: [
-          {
-            loader: 'file-loader'
-          }
-        ]
-      }
-    ]
-  },
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  },
-  plugins: [
-    new WriteFilePlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
-  ],
-  watchOptions: {
-    ignored: ['/node_modules/']
-  }
+            {
+              loader: 'postcss-loader',
+              options: {
+                autoprefixer: {
+                  browsers: ['last 2 versions']
+                },
+                plugins: () => [autoprefixer]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {}
+            }
+          ]
+        },
+        {
+          test: /\.(ico|gif|png|jpe?g|svg)$/,
+          loaders: [
+            {
+              loader: 'file-loader'
+            }
+          ]
+        }
+      ]
+    },
+    optimization: {
+      minimizer: [new OptimizeCSSAssetsPlugin({})]
+    },
+    plugins: [
+      new WriteFilePlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      })
+    ],
+    watchOptions: {
+      ignored: ['/node_modules/']
+    }
+  };
 };
