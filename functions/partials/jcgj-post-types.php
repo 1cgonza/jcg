7cbee98e1fe49475d2d5fc6b48655519cac2cd56
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-12-10 17:06:35 -0500

    ✨ Exponer CPT a graphql

--- functions/partials/jcgj-post-types.php
+++ functions/partials/jcgj-post-types.php
@@ -31,6 +31,45 @@ register_post_type( 'films',
     'supports'            => array('title', 'author', 'thumbnail'),
     'taxonomies'          => array('category', 'post_tag'),
     'show_in_rest'        => true,
+    'show_in_graphql'     => true,
+    'graphql_single_name' => 'film',
+    'graphql_plural_name' => 'films'
+	)
+);
+
+register_post_type( 'research',
+	array(
+    'labels' => array(
+      'name'               => 'Research',
+      'singular_name'      => 'Research',
+      'all_items'          => 'All Research',
+      'add_new'            => 'Add New',
+      'add_new_item'       => 'Add New Research',
+      'edit'               => 'Edit',
+      'edit_item'          => 'Edit Research',
+      'new_item'           => 'New Research',
+      'view_item'          => 'View Research',
+      'search_items'       => 'Search Research',
+      'not_found'          => 'Nothing found in the Database.',
+      'not_found_in_trash' => 'Nothing found in Trash',
+      'parent_item_colon'  => ''
+    ),
+    'description'         => 'The research post type with custom display.',
+    'public'              => true,
+    'publicly_queryable'  => true,
+    'exclude_from_search' => false,
+    'show_ui'             => true,
+    'query_var'           => true,
+    'menu_icon'           => 'dashicons-visibility',
+    'has_archive'         => true,
+    'capability_type'     => 'post',
+    'hierarchical'        => false,
+    'supports'            => array('title', 'author', 'thumbnail'),
+    'taxonomies'          => array('category', 'post_tag'),
+    'show_in_rest'        => true,
+    'show_in_graphql'     => true,
+    'graphql_single_name' => 'research',
+    'graphql_plural_name' => 'researchProjects'
 	)
 );
 
@@ -63,25 +102,28 @@ register_post_type( 'cv_meta',
     'hierarchical'        => false,
     'supports'            => array('title', 'author', 'thumbnail'),
     'show_in_rest'        => true,
+    'show_in_graphql'     => true,
+    'graphql_single_name' => 'cv',
+    'graphql_plural_name' => 'cvs'
 	)
 );
 
 register_post_type( 'experiments',
 	array(
 		'labels' => array(
-      'name'               => 'Experiments',
-      'singular_name'      => 'Experiment',
-      'all_items'          => 'All Experiments',
-      'add_new'            => 'Add New',
-      'add_new_item'       => 'Add New Experiment',
-      'edit'               => 'Edit',
-      'edit_item'          => 'Edit Experiment',
-      'new_item'           => 'New Experiment',
-      'view_item'          => 'View Experiment',
-      'search_items'       => 'Search Experiment',
-      'not_found'          => 'Nothing found in the Database.',
-      'not_found_in_trash' => 'Nothing found in Trash',
-      'parent_item_colon'  => ''
+         'name'               => 'Experiments',
+         'singular_name'      => 'Experiment',
+         'all_items'          => 'All Experiments',
+         'add_new'            => 'Add New',
+         'add_new_item'       => 'Add New Experiment',
+         'edit'               => 'Edit',
+         'edit_item'          => 'Edit Experiment',
+         'new_item'           => 'New Experiment',
+         'view_item'          => 'View Experiment',
+         'search_items'       => 'Search Experiment',
+         'not_found'          => 'Nothing found in the Database.',
+         'not_found_in_trash' => 'Nothing found in Trash',
+         'parent_item_colon'  => ''
 		),
 		'description'         => 'LAB.',
 		'public'              => true,
@@ -95,26 +137,29 @@ register_post_type( 'experiments',
 		'hierarchical'        => false,
 		'supports'            => array('title', 'editor', 'author', 'thumbnail'),
 		'taxonomies'          => array('post_tag'),
-    'show_in_rest'        => true,
+      'show_in_rest'        => true,
+      'show_in_graphql'     => true
+      'graphql_single_name' => 'experiment',
+      'graphql_plural_name' => 'experiments'
 	)
 );
 
 register_post_type( 'commissions',
 	array(
 		'labels' => array(
-      'name'               => 'Comissions',
-      'singular_name'      => 'Comission',
-      'all_items'          => 'All Comissions',
-      'add_new'            => 'Add New',
-      'add_new_item'       => 'Add New Comission',
-      'edit'               => 'Edit',
-      'edit_item'          => 'Edit Comission',
-      'new_item'           => 'New Comission',
-      'view_item'          => 'View Comission',
-      'search_items'       => 'Search Comissions',
-      'not_found'          => 'Nothing found in the Database.',
-      'not_found_in_trash' => 'Nothing found in Trash',
-      'parent_item_colon'  => ''
+         'name'               => 'Commissions',
+         'singular_name'      => 'Commission',
+         'all_items'          => 'All Commissions',
+         'add_new'            => 'Add New',
+         'add_new_item'       => 'Add New Commission',
+         'edit'               => 'Edit',
+         'edit_item'          => 'Edit Commission',
+         'new_item'           => 'New Commission',
+         'view_item'          => 'View Commission',
+         'search_items'       => 'Search Commissions',
+         'not_found'          => 'Nothing found in the Database.',
+         'not_found_in_trash' => 'Nothing found in Trash',
+         'parent_item_colon'  => ''
 		),
 		'description'         => 'Commissioned projects.',
 		'public'              => true,
@@ -128,7 +173,10 @@ register_post_type( 'commissions',
 		'hierarchical'        => false,
 		'supports'            => array('title', 'author', 'thumbnail'),
 		'taxonomies'          => array('post_tag'),
-    'show_in_rest'        => true,
+      'show_in_rest'        => true,
+      'show_in_graphql'     => true,
+      'graphql_single_name' => 'commission',
+      'graphql_plural_name' => 'commissions'
 	)
 );
 
@@ -137,21 +185,24 @@ register_taxonomy('cv_cat',
 	array(
 		'hierarchical' => true,
 		'labels'       => array(
-      'name'              => 'CV Categories',
-      'singular_name'     => 'CV Category',
-      'search_items'      => 'Search CV Categories',
-      'all_items'         => 'All CV Categories',
-      'parent_item'       => 'Parent CV Category',
-      'parent_item_colon' => 'Parent CV Category:',
-      'edit_item'         => 'Edit CV Category',
-      'update_item'       => 'Update CV Category',
-      'add_new_item'      => 'Add New CV Category',
-      'new_item_name'     => 'New CV Category Name',
+         'name'              => 'CV Categories',
+         'singular_name'     => 'CV Category',
+         'search_items'      => 'Search CV Categories',
+         'all_items'         => 'All CV Categories',
+         'parent_item'       => 'Parent CV Category',
+         'parent_item_colon' => 'Parent CV Category:',
+         'edit_item'         => 'Edit CV Category',
+         'update_item'       => 'Update CV Category',
+         'add_new_item'      => 'Add New CV Category',
+         'new_item_name'     => 'New CV Category Name',
 		),
 		'show_admin_column' => true,
 		'show_ui'           => true,
 		'query_var'         => true,
 		'rewrite'           => array( 'slug' => 'cv' ),
+      'show_in_graphql'     => true,
+      'graphql_single_name' => 'cvCat',
+      'graphql_plural_name' => 'cvCats'
 	)
 );
 
@@ -160,20 +211,23 @@ register_taxonomy( 'commission_cat',
 	array(
 		'hierarchical' => true,
 		'labels'       => array(
-      'name'              => 'Comission Categories',
-      'singular_name'     => 'Comission Category',
-      'search_items'      => 'Search Comission Categories',
-      'all_items'         => 'All Comission Categories',
-      'parent_item'       => 'Parent Comission Category',
-      'parent_item_colon' => 'Parent Comission Category:',
-      'edit_item'         => 'Edit Comission Category',
-      'update_item'       => 'Update Comission Category',
-      'add_new_item'      => 'Add New Comission Category',
-      'new_item_name'     => 'New Comission Category Name',
+         'name'              => 'Commission Categories',
+         'singular_name'     => 'Commission Category',
+         'search_items'      => 'Search Commission Categories',
+         'all_items'         => 'All Commission Categories',
+         'parent_item'       => 'Parent Commission Category',
+         'parent_item_colon' => 'Parent Commission Category:',
+         'edit_item'         => 'Edit Commission Category',
+         'update_item'       => 'Update Commission Category',
+         'add_new_item'      => 'Add New Commission Category',
+         'new_item_name'     => 'New Commission Category Name',
 		),
 		'show_admin_column' => true,
 		'show_ui'           => true,
 		'query_var'         => true,
-		'rewrite'           => array( 'slug' => 'comission' )
+		'rewrite'           => array( 'slug' => 'commission' ),
+      'show_in_graphql'     => true,
+      'graphql_single_name' => 'commissionCat',
+      'graphql_plural_name' => 'commissionCats'
 	)
 );
